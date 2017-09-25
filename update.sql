cf610ea91f8034fbc24a326897781510e88a28b8
github username <github email address>
2017-09-24 23:28:17 -0400

    Done.

---
+++ update.sql
@@ -0,0 +1,5 @@
+SQLite version 3.16.0 2016-11-04 19:09:39
+Enter ".help" for usage hints.
+Connected to a [1mtransient in-memory database[0m.
+Use ".open FILENAME" to reopen on a persistent database.
+
